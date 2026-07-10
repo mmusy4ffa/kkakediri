00c1069ea7c4699880652dc865fb4091dda2075c
Muhammad Musyaffa <llgaman008@gmail.com>
2026-07-10 22:57:08 +0700

    vercellll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: false },
   }),
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
